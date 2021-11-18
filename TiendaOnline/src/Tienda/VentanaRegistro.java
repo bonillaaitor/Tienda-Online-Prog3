1d014538a541ece80ae2a364677970742f51d23a
jonsoler <jon.soler@opendeusto.es>
2021-11-18 16:05:55 +0100

    Corrección campos VentanaRegistro

--- TiendaOnline/src/Tienda/VentanaRegistro.java
+++ TiendaOnline/src/Tienda/VentanaRegistro.java
@@ -59,31 +59,31 @@ public class VentanaRegistro extends JFrame {
 		contentPanel.setLayout(null);
 
 		JLabel labelNombre = new JLabel("Nombre");
-		labelNombre.setBounds(70, 45, 46, 14);
+		labelNombre.setBounds(70, 80, 46, 14);
 		contentPanel.add(labelNombre);
 
 		JLabel labelUsuario = new JLabel("Usuario");
-		labelUsuario.setBounds(70, 86, 46, 14);
+		labelUsuario.setBounds(70, 125, 46, 14);
 		contentPanel.add(labelUsuario);
 
-		JLabel labelPassword = new JLabel("Password");
-		labelPassword.setBounds(70, 131, 46, 14);
+		JLabel labelPassword = new JLabel("Contraseña");
+		labelPassword.setBounds(70, 170, 80, 14);
 		contentPanel.add(labelPassword);
 
 		JLabel labelCorreo = new JLabel("Correo");
-		labelCorreo.setBounds(70, 177, 46, 14);
+		labelCorreo.setBounds(70, 215, 46, 14);
 		contentPanel.add(labelCorreo);
 
 		JLabel labelDireccion = new JLabel("Direccion");
-		labelDireccion.setBounds(70, 224, 46, 14);
+		labelDireccion.setBounds(70, 260, 80, 14);
 		contentPanel.add(labelDireccion);
 
 		JLabel labelTelefono = new JLabel("Telefono");
-		labelTelefono.setBounds(70, 267, 46, 14);
+		labelTelefono.setBounds(70, 305, 80, 14);
 		contentPanel.add(labelTelefono);
 
 		JLabel labelTarjeta = new JLabel("Tarjeta\r\n");
-		labelTarjeta.setBounds(70, 315, 46, 14);
+		labelTarjeta.setBounds(70, 350, 46, 14);
 		contentPanel.add(labelTarjeta);
 
 		JButton botonRegistroDos = new JButton("Registrarme");
@@ -97,43 +97,43 @@ public class VentanaRegistro extends JFrame {
 		contentPanel.add(botonAtras);
 
 		textoNombre = new JTextField();
-		textoNombre.setBounds(148, 42, 164, 20);
+		textoNombre.setBounds(155, 78, 164, 20);
 		contentPanel.add(textoNombre);
 		textoNombre.setColumns(10);
 
 		textoUsuario = new JTextField();
-		textoUsuario.setBounds(148, 83, 164, 20);
+		textoUsuario.setBounds(155, 123, 164, 20);
 		contentPanel.add(textoUsuario);
 		textoUsuario.setColumns(10);
 
 		textoPassword = new JTextField();
-		textoPassword.setBounds(148, 128, 164, 20);
+		textoPassword.setBounds(155, 168, 164, 20);
 		contentPanel.add(textoPassword);
 		textoPassword.setColumns(10);
 
 		textoCorreo = new JTextField();
-		textoCorreo.setBounds(148, 174, 164, 20);
+		textoCorreo.setBounds(155, 213, 164, 20);
 		contentPanel.add(textoCorreo);
 		textoCorreo.setColumns(10);
 
 		textoDireccion = new JTextField();
-		textoDireccion.setBounds(148, 221, 164, 20);
+		textoDireccion.setBounds(155, 258, 164, 20);
 		contentPanel.add(textoDireccion);
 		textoDireccion.setColumns(10);
 
 		textoTelefono = new JTextField();
-		textoTelefono.setBounds(148, 264, 164, 20);
+		textoTelefono.setBounds(155, 303, 164, 20);
 		contentPanel.add(textoTelefono);
 		textoTelefono.setColumns(10);
 
 		textoTarjeta = new JTextField();
-		textoTarjeta.setBounds(148, 312, 164, 20);
+		textoTarjeta.setBounds(155, 348, 164, 20);
 		contentPanel.add(textoTarjeta);
 		textoTarjeta.setColumns(10);
 
-		JLabel labelRegistro = new JLabel("Registrarse en nuestra tienda online");
+		JLabel labelRegistro = new JLabel("Registrate en nuestra tienda online");
 		labelRegistro.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 20));
-		labelRegistro.setBounds(59, 10, 387, 20);
+		labelRegistro.setBounds(59, 10, 387, 30);
 		contentPanel.add(labelRegistro);
 
 	}
